671a24e144404495c8b37e380693113bf5907fb8
HarvWare <181854019+Nharvey00@users.noreply.github.com>
2025-09-08 21:43:07 +0800

    Update SinglyLinkedList.java

--- src/singly_linked_list/SinglyLinkedList.java
+++ src/singly_linked_list/SinglyLinkedList.java
@@ -2,38 +2,38 @@ package singly_linked_list;
 
 public class SinglyLinkedList<Z> implements SinglyLinkedListInterface<Z> {
 	
-	private Node<Z> head;
+	private Node<Z> head; //attributes that describe what nodes contain and how many
 	private Node<Z> tail;
 	private int size;
 	
-	public SinglyLinkedList() {
+	public SinglyLinkedList() { //
 		head = null;
 		tail = null;
 		size = 0;
 	}
 	
-	public int size() {
+	public int size() { //returns the current number of nodes XD
 		return size;
 	}
 	
-	public boolean isEmpty() {
+	public boolean isEmpty() { //kabalo namo ani hehehe hart hart
 		return size == 0;
 	}
 	
-	public void clear() {
+	public void clear() { //self explanatory
 		head = null;
 		tail = null;
 		size = 0;
 	}
 	
-	public void addFirst(Z item) {
+	public void addFirst(Z item) { 
 		Node<Z> newNode = new Node<>(item);
 		if (isEmpty()) {
 			head = newNode;
 			tail = newNode;
 		} else {
-			newNode.next = head;
-			head = newNode;
+			newNode.next = head; //added node points to the current head node
+			head = newNode; //the added node becomes the new head
 		}
 		size++;
 	}
@@ -44,8 +44,8 @@ public class SinglyLinkedList<Z> implements SinglyLinkedListInterface<Z> {
 			head = newNode;
 			tail = newNode;
 		} else {
-			tail.next = newNode;
-			tail = newNode;
+			tail.next = newNode; //tail node points to the added node
+			tail = newNode; //added node becomes the new tail node
 		}
 		size++;
 	}
@@ -72,7 +72,7 @@ public class SinglyLinkedList<Z> implements SinglyLinkedListInterface<Z> {
 			current = current.next;
 		}
 		
-		newNode.next = current.next;
+		newNode.next = current.next; //explain ra nako ugma hahahaahhaahahaah
 		current.next = newNode;
 		
 		size++;
@@ -99,48 +99,103 @@ public class SinglyLinkedList<Z> implements SinglyLinkedListInterface<Z> {
 
 	@Override
 	public Z removeLast() {
+		if (isEmpty()) {
+			throw new IllegalStateException("List is empty");
+		}
+		Z removedData = tail.data;
 		
-		// TODO Auto-generated method stub
-		return null;
+		if (head == tail) { // only one element
+			head = null;
+			tail = null;
+		} else {
+			Node<Z> current = head;
+			while (current.next != tail) {
+				current = current.next;
+			}
+			current.next = null; //ubuson nya tanan ka lantaw ang list asta maabot sya sa before sang tail kag ang next sina himuon nya null
+			tail = current;
+		}
+		
+		size--;
+		return removedData;
 	}
 
 	@Override
 	public Z remove(int index, Z item) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+		}
+		
+		if (index == 0) return removeFirst();
+		if (index == size - 1) return removeLast();
+		
+		Node<Z> current = head;
+		for (int i = 0; i < index - 1; i++) {
+			current = current.next;
+		}
+		Z removedData = current.next.data;
+		current.next = current.next.next;
+		size--;
+		return removedData;
 	}
 
 	@Override
 	public boolean removeItem(Z item) {
-		// TODO Auto-generated method stub
+		if (isEmpty()) return false;
+		
+		if (head.data.equals(item)) {
+			removeFirst();
+			return true;
+		}
+		
+		Node<Z> current = head;
+		while (current.next != null) {
+			if (current.next.data.equals(item)) {
+				if (current.next == tail) {
+					tail = current;
+				}
+				current.next = current.next.next;
+				size--;
+				return true;
+			}
+			current = current.next;
+		}
 		return false;
 	}
 
 	@Override
 	public Z getFirst() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) throw new IllegalStateException("List is empty");
+		return head.data;
 	}
 
 	@Override
 	public Z getLast() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) throw new IllegalStateException("List is empty");
+		return tail.data;
 	}
 
 	@Override
 	public Z get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+		}
+		Node<Z> current = head;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		return current.data;
 	}
 
 	@Override
 	public int indexOf(Z item) {
-		// TODO Auto-generated method stub
-		return 0;
+		Node<Z> current = head;
+		int index = 0;
+		while (current != null) {
+			if (current.data.equals(item)) return index;
+			current = current.next;
+			index++;
+		}
+		return -1; // not found
 	}
-	
-	
-	
-
 }
