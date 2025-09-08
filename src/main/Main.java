b53050ee02b46fb9fbfb7ba3c7b2405919839972
HarvWare <181854019+Nharvey00@users.noreply.github.com>
2025-09-08 21:44:55 +0800

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,22 @@
 package main;
+import java.util.Scanner;
+import singly_linked_list.Node;
+import singly_linked_list.SinglyLinkedList;
+import singly_linked_list.SinglyLinkedListInterface;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		SinglyLinkedListInterface s = new SinglyLinkedList();
+		
+		String input = sc.nextLine();
+		s.addFirst(input);
+		
+		System.out.println(s.getFirst());
+		
 
 	}
 
