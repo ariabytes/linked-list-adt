76d4523fcdaf8885370756d97e0c39c8dbe0a63f
ramillanomark <ramillanomarky@gmail.com>
2025-09-09 16:50:17 +0800

    Update sa main.java
    katong mga linkedlist na gipang butang ganina

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,13 +11,41 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		SinglyLinkedListInterface s = new SinglyLinkedList();
+		System.out.println(s.size());
+		s.addFirst("balay ni mayang");
 		
-		String input = sc.nextLine();
-		s.addFirst(input);
+		System.out.println(s.size());
+		s.add(1, "nicki minaj");
+		s.add(2, "anaconda");
+		s.add(3, "ahhhh");
+		s.add(4, "mark");
+		s.add(5, "wowow");
+		s.add(6, "tail");
+		s.addLast("last");
 		
 		System.out.println(s.getFirst());
+		System.out.println(s.getLast());
+		System.out.println(s.get(1));
+		System.out.println(s.indexOf("ahhhh"));
 		
+		System.out.println(s.toString());
+		
+		s.removeLast();
 
+		System.out.println(s.toString());
+		
+		s.removeFirst();
+
+		System.out.println(s.toString());
+		
+		s.add(3, "index 3");
+		s.add(5, "index 5");
+
+
+		System.out.println(s.toString());
+		System.out.println(s.size());
+
+		
 	}
 
 }
